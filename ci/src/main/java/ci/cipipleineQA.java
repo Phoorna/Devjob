2b6ec3900b9c6fd0461e65c646c33c9cb49e2f43
Ravichandran <PRavichandran@teampurpose.com>
2021-08-18 16:49:46 -0400

    testing line3

--- ci/src/main/java/ci/cipipleineQA.java
+++ ci/src/main/java/ci/cipipleineQA.java
@@ -6,6 +6,8 @@ public class cipipleineQA {
 		// TODO Auto-generated method stub
 		
 		System.out.println("adding for workflow pipeline");
+		System.out.println("adding for workflow pipeline2");
+
 
 	}
 
