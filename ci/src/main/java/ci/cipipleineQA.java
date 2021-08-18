4f34afc27f0dc756ba67f82738cd968f337559ad
Ravichandran <PRavichandran@teampurpose.com>
2021-08-18 16:38:19 -0400

    testing

---
+++ ci/src/main/java/ci/cipipleineQA.java
@@ -0,0 +1,12 @@
+package ci;
+
+public class cipipleineQA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("adding for workflow pipeline");
+
+	}
+
+}
